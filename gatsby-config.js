22e0395a82c6fc5d19192354d998eac46624d5ad
Yusuff Faruq <faruqyusuff437@gmail.com>
2019-11-23 19:23:50 +0100

    Changed background colors

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         short_name: `KatanaBlog`,
         start_url: `/`,
         background_color: `#fb9879`,
-        theme_color: `#1d72a1`,
+        theme_color: `#fb9879`,
         display: `minimal-ui`,
         icon: `src/images/luck.svg`, // This path is relative to the root of the site.
       },
